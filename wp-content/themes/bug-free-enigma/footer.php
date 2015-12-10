3b235aa3887d4fbb6d2bc00bad0eb95baa7a6687
bri <hello@bripiccari.me>
2015-12-09 21:33:31 -0500

    base theme files.

---
+++ wp-content/themes/bug-free-enigma/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package bug-free-enigma
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bripiccari-me' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'bripiccari-me' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'bripiccari-me' ), 'bripiccari-me', '<a href="http://bripiccari.me" rel="designer">Bri Piccari</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>

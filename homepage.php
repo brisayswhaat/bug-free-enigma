b66cec4a09c6404ba8d3ea72c3ea0df1f00d2ca7
Bri <hello@bripiccari.me>
2015-12-29 19:31:39 -0500

    add homepage file

---
+++ homepage.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+* Template Name: Homepage Template File
+* @package bug-free-enigma
+*
+*/
+
+get_header(); ?>
+
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+    </main>
+</div>
+
+<?php get_footer(); ?>

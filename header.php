b32c7dd1f491aa3370a59071539ef580ae1d425e
Bri <hello@bripiccari.me>
2016-02-14 09:03:45 -0500

    Remove unnecessary crap from header. Export new stylesheet. Fresh slate.

--- header.php
+++ header.php
@@ -1,50 +1,25 @@
 <?php
 /**
- * The header for our theme.
- *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package bug-free-enigma-v2
- */
+  * The header for our theme
+  * This is the template that displays all of the <head> section and everything up until <div id="content">
+  *
+  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+  * @package bug-free-enigma
+  */
 
-?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-<meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="profile" href="http://gmpg.org/xfn/11">
-<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+  ?> <!DOCTYPE html>
+  <html <?php language_attributes(); ?>
+  <head>
+	  <meta charset="<?php bloginfo( 'charset' ); ?>">
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	  <title>Bri Piccari | Designer & Front End Developer</title>
 
-<?php wp_head(); ?>
-</head>
+	  <link rel="profile" href="http://gmpg.org/xfn/11">
+	  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+	  <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+	  <link href='https://fonts.googleapis.com/css?family=Merriweather:700,400,900,400italic,700italic' rel='stylesheet' type='text/css'>
 
-<body <?php body_class(); ?>>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'httpwww-bripiccari-me' ); ?></a>
-
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<?php
-			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-			<?php
-			endif;
-
-			$description = get_bloginfo( 'description', 'display' );
-			if ( $description || is_customize_preview() ) : ?>
-				<p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
-			<?php
-			endif; ?>
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'httpwww-bripiccari-me' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+	  <?php wp_head(); ?>
+  </head>

725b2a1cde7c119720847a14fa6214ffaa2dcbea
Bri <hello@bripiccari.me>
2016-02-14 10:12:37 -0500

    Revised header.php file.

--- header.php
+++ header.php
@@ -23,3 +23,18 @@
 
 	  <?php wp_head(); ?>
   </head>
+	<body <?php body_class(); ?>
+	<div id="page" class="site">
+		<header id="masthead" class="site-header" role="banner">
+			<nav id="navigation" class="main-navigation" role="navigation">
+				<ul>
+					<li>
+						<a href="/" title="Go Home"  class="main-navigation-link"><i class="fa fa-home"></i></a>
+					</li>
+					<li>
+						<a href="/words" title="Blog" class="main-navigation-link">/ words</a>
+					</li>
+				</ul>
+		</header>
+
+		<div id="content" class="site-content">

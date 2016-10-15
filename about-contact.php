9e42d05a9d24fae55597a90893fa6d5fcbb9169f
Bri <hello@bripiccari.me>
2016-10-15 19:50:57 -0400

    more about page styles! two columns. you fancy.

--- about-contact.php
+++ about-contact.php
@@ -9,59 +9,60 @@ get_header(); ?>
 <main id="main" class="site-main" role="main">
   <section class="thats-a-wrap">
 
-      <div class="content--homepage">
-        <?php
-          if ( have_posts() ) {
-            while ( have_posts() ) {
-              the_post();
-              the_content();
-            } // end while
-          } // end if
-        ?>
+      <div class="content--columns">
+        <div class="column">
+          <?php
+            if ( have_posts() ) {
+              while ( have_posts() ) {
+                the_post();
+                the_content();
+              } // end while
+            } // end if
+          ?>
+        </div>
 
-        <h2 class="subheadline">Find Me On:</h2>
-        <ul class="social-links">
-          <li>
-          <a href="mailto:hello@bripiccari.me" target="_blank" title="Email" class="icon">
-            <i class="fa fa-envelope"></i>
-          </a>
-          </li>
-          <li>
-          <a href="http://www.twitter.com/brisayswhaat" target="_blank" title="Twitter" class="icon">
-            <i class="fa fa-twitter"></i>
-          </a>
-          </li>
-          <li>
-          <a href="http://www.instagram.com/brisayswhaat" target="_blank" title="Instagram" class="icon">
-            <i class="fa fa-instagram"></i>
-          </a>
-          </li>
-          <li>
-          <a href="https://dribbble.com/brisayswhaat" target="_blank" title="Dribbble" class="icon">
-            <i class="fa fa-dribbble"></i>
-          </a>
-          </li>
-          <li>
-            <a href="https://github.com/brisayswhaat" target="_blank" title="Github" class="icon">
-              <i class="fa fa-github"></i>
+        <div class="column">
+          <h2 class="subheadline">Find Me On:</h2>
+          <ul class="social-links">
+            <li>
+            <a href="http://www.twitter.com/brisayswhaat" target="_blank" title="Twitter" class="icon">
+              <i class="fa fa-twitter"></i>
             </a>
-          </li>
-          <li>
-          <a href="http://www.pinterest.com/brisayswhaat" target="_blank" title="Pinterest" class="icon">
-            <i class="fa fa-pinterest"></i>
-          </a>
-          </li>
-          <li>
-          <a href="https://open.spotify.com/user/1215731033" target="_blank" title="Spotify" class="icon">
-            <i class="fa fa-spotify"></i>
-          </a>
-          </li>
-          <li>
-          <a href="https://www.linkedin.com/in/bripiccari" target="_blank" title="LinkedIn" class="icon">
-            <i class="fa fa-linkedin"></i>
-          </a>
-          </li>
-        </ul>
+            </li>
+            <li>
+            <a href="http://www.instagram.com/brisayswhaat" target="_blank" title="Instagram" class="icon">
+              <i class="fa fa-instagram"></i>
+            </a>
+            </li>
+            <li>
+            <a href="https://dribbble.com/brisayswhaat" target="_blank" title="Dribbble" class="icon">
+              <i class="fa fa-dribbble"></i>
+            </a>
+            </li>
+            <li>
+              <a href="https://github.com/brisayswhaat" target="_blank" title="Github" class="icon">
+                <i class="fa fa-github"></i>
+              </a>
+            </li>
+            </li>
+            <li>
+            <a href="https://open.spotify.com/user/1215731033" target="_blank" title="Spotify" class="icon">
+              <i class="fa fa-spotify"></i>
+            </a>
+            </li>
+            <li>
+            <a href="https://www.linkedin.com/in/bripiccari" target="_blank" title="LinkedIn" class="icon">
+              <i class="fa fa-linkedin"></i>
+            </a>
+            </li>
+          </ul>
+
+          <div class="contact-me">
+            <h2 class="subheadline">Get In Touch:</h2>
+            <a href="mailto:hello@bripiccari.me" title="Shoot me an Email" class="link sans button">Email Me</a>
+
+          </div>
+        </div>
     </div>
   </section>
 </main>

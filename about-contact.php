db9b7c1e7bdf67f9c164db78dfa3161f2b146f66
Bri <hello@bripiccari.me>
2016-10-15 19:24:17 -0400

    update normalize to wipe out ul, li padding in chrome. add social links from footer to about/contact page.

--- about-contact.php
+++ about-contact.php
@@ -20,7 +20,48 @@ get_header(); ?>
         ?>
 
         <h2 class="subheadline">Find Me On:</h2>
-
+        <ul class="social-links">
+          <li>
+          <a href="mailto:hello@bripiccari.me" target="_blank" title="Email" class="icon">
+            <i class="fa fa-envelope"></i>
+          </a>
+          </li>
+          <li>
+          <a href="http://www.twitter.com/brisayswhaat" target="_blank" title="Twitter" class="icon">
+            <i class="fa fa-twitter"></i>
+          </a>
+          </li>
+          <li>
+          <a href="http://www.instagram.com/brisayswhaat" target="_blank" title="Instagram" class="icon">
+            <i class="fa fa-instagram"></i>
+          </a>
+          </li>
+          <li>
+          <a href="https://dribbble.com/brisayswhaat" target="_blank" title="Dribbble" class="icon">
+            <i class="fa fa-dribbble"></i>
+          </a>
+          </li>
+          <li>
+            <a href="https://github.com/brisayswhaat" target="_blank" title="Github" class="icon">
+              <i class="fa fa-github"></i>
+            </a>
+          </li>
+          <li>
+          <a href="http://www.pinterest.com/brisayswhaat" target="_blank" title="Pinterest" class="icon">
+            <i class="fa fa-pinterest"></i>
+          </a>
+          </li>
+          <li>
+          <a href="https://open.spotify.com/user/1215731033" target="_blank" title="Spotify" class="icon">
+            <i class="fa fa-spotify"></i>
+          </a>
+          </li>
+          <li>
+          <a href="https://www.linkedin.com/in/bripiccari" target="_blank" title="LinkedIn" class="icon">
+            <i class="fa fa-linkedin"></i>
+          </a>
+          </li>
+        </ul>
     </div>
   </section>
 </main>

cad2e7a90579f5a225a577e305482a8dd66cb293
Bri <hello@bripiccari.me>
2016-10-15 15:09:41 -0400

    add new template for about page.

---
+++ about-contact.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* Template Name: Homepage Template File
+* @package bug-free-enigma
+*
+*/
+get_header(); ?>
+
+<main id="primary" class="content-area site-main" role="main">
+
+
+
+
+
+
+
+
+
+
+
+
+</main>
+</php get_footer(); ?>

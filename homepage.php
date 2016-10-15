754f74b22bc20840aabba2222433606949aabc94
Bri <hello@bripiccari.me>
2016-10-15 15:55:02 -0400

    homepage styles. set #teamgradient.

--- homepage.php
+++ homepage.php
@@ -6,28 +6,27 @@
 */
 
 get_header(); ?>
-
-
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
 
         <section class="hero">
             <div class="outer-container">
-                <h1 class="headline headline-largest">Hello, Bri here.</h1>
-                <ul class="headline">
+
+
+
+
                     <?php
                     if ( have_posts() ) {
                     	while ( have_posts() ) {
                     		the_post();
-                    		the_content(); 
+                    		the_content();
                     	} // end while
                     } // end if
                     ?>
-                </ul>
+
+
             </div>
         </section>
 
     </main>
 </div>
-
-<?php get_footer(); ?>

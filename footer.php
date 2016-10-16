ddd99bec4cc0640f63a8bf4f03d5e72b1c0f7ed5
Bri <hello@bripiccari.me>
2016-10-16 19:20:02 -0400

    footer build. add max-width to header.

--- footer.php
+++ footer.php
@@ -12,15 +12,14 @@
 
  <footer id="colophon" class="site-footer footer" role="contentinfo">
 	 <div class="site-info footer-copy">
-		 Made with <i class="fa fa-heart"></i> in Lancaster, Pennsylvania. <br>
-		 <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bripiccari-me' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'bripiccari-me' ), 'WordPress' ); ?></a> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; &copy; 2016 Bri Piccari. All Rights Reserved.
+		 <p>Made with <i class="fa fa-heart"></i> in Lancaster, Pennsylvania. <br>
+		 <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bripiccari-me' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'bripiccari-me' ), 'WordPress' ); ?></a> &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp; &copy; 2016 Bri Piccari. All Rights Reserved.</p>
 	 </div><!-- .site-info -->
  </footer> <!-- #colophon -->
 
  </div> <!-- #page -->
 
 <?php wp_footer(); ?>
-
 
 <script src="https://use.fontawesome.com/634e437d8d.js"></script>
 <script src="https://use.typekit.net/swx1oyy.js"></script>

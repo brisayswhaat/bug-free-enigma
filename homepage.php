7d55c400558692c374ae4d417bca31db48261c71
Bri <hello@bripiccari.me>
2016-10-16 19:31:50 -0400

    add max-width to content.

--- homepage.php
+++ homepage.php
@@ -8,18 +8,15 @@
 get_header(); ?>
 
 <main id="main" class="site-main" role="main">
-  <section class="thats-a-wrap">
-
-      <div class="content--homepage">
-        <?php
-          if ( have_posts() ) {
-            while ( have_posts() ) {
-              the_post();
-              the_content();
-            } // end while
-          } // end if
-        ?>
-      </div>
+  <section class="wrapped">
+    <?php
+      if ( have_posts() ) {
+        while ( have_posts() ) {
+          the_post();
+          the_content();
+        } // end while
+      } // end if
+    ?>
   </section>
 </main>
 

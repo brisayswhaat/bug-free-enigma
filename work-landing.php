2a0733b20280ff8453e750637c789ea94976e7b4
Bri <hello@bripiccari.me>
2016-10-17 21:24:37 -0400

    word landing page template. fixed footer.

---
+++ work-landing.php
@@ -0,0 +1,40 @@
+<?php
+/**
+* Template Name: Work Landing Template File
+* @package bug-free-enigma
+*
+*/
+get_header(); ?>
+
+<main id="main" class="site-main" role="main">
+  <section class="wrapped">
+
+    <a href="http://lancasterufi.org" title="Lancaster Urban Farming Initiative" class="">Lancaster Urban Farming Initiative</a>
+    <p class="">Designer, Developer</p>
+
+    <a href="http://assetspa.org" title="Assets PA">ASSETS PA</a>
+    <p class="">Designer, Developer</p>
+
+    <a href="http://sandtshoney.com" title="Sandt's Honey">Sandt's Honey</a>
+    <p class="">Designer, Developer</p>
+
+    <a href="http://triode.tv" title="Triode TV">Triode TV</a>
+    <p class="">Designer, Developer</p>
+
+    <a href="/work/pledge-it" title="PLEDGE IT">PLEDGE IT</a>
+    <p class="">Designer, Developer</p>
+
+    <a href="http://www.dribbble.com/brisayswhaat/getting-away-with-shit" title="Getting Away With Shit">Getting Away With Shit</a>
+    <p class="">Lettering, Designer</p>
+
+    <a href="http://www.dribbble.com/brisayswhaat/recharge" title="ReCharge">ReCharge</a>
+    <p class="">Branding</p>
+
+    <a href="http://www.dribbble.com/brisayswhaat/coffee-and-critique" title="Coffee and Critique">Coffee and Critique 2.0</a>
+    <p class="">Branding, Illustration</p>
+
+    <a href="http://www.dribbble.com/brisayswhaat" title="Misc. Other Projects">Misc.</a>
+
+  </section>
+</main>
+<?php get_footer(); ?>

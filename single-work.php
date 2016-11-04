56cacb13446d8eed7f13dbda43219c85589bec31
Bri <hello@bripiccari.me>
2016-11-04 07:31:05 -0400

    single work place template.

---
+++ single-work.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* Template Name: Single Work Page
+* @package bug-free-enigma
+*
+*/
+get_header(); ?>
+
+<main id="main" class="site-main" role="main">
+  <section class="wrapped">
+    <div class="about full-width">
+
+    </div>
+
+
+
+
+
+  </section>
+</main>
+
+<?php get_footer(); ?>

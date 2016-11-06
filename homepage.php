54334ec9829ea3111036e3e0955edfa2334fcf13
Bri <hello@bripiccari.me>
2016-11-06 18:17:36 -0500

    mobile styles b/c i'm backwards today. oops.

--- homepage.php
+++ homepage.php
@@ -10,15 +10,12 @@ get_header(); ?>
 <main id="main" class="site-main" role="main">
   <section class="homepage-content">
 
-    <?php
-      if ( have_posts() ) {
-        while ( have_posts() ) {
-          the_post();
-          the_content();
-        } // end while
-      } // end if
-    ?>
-    
+    <p class="copy larger"><strong>CURRENTLY:</strong></p>
+    <p class="copy larger">President of <a title="AIGA Central Pennsylvania" href="http://centralpa.aiga.org" target="_blank">AIGA Central Pennsylvania</a></p>
+    <p class="copy larger">Designer &amp; Developer at <a href="https://www.theindustrialresolution.com/" target="_blank">Indy Res</a></p>
+    <p class="copy larger">Chief Kitten Officer at <a title="PHLDesignCo" href="http://phldesign.co" target="_blank">PHLDesignCo</a></p>
+    <p class="copy larger"><a href="/about-contact" title="More about me">And then some.</a></p>
+
   </section>
 </main>
 

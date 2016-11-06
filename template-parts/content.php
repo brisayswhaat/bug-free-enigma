4da01e3dccd0d9459e8ab7e0de45abe06100e9ab
Bri <hello@bripiccari.me>
2016-11-06 17:51:57 -0500

    desktop done. adjust typefaces across site. i did too much. i forgot to commit. YOLO.

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,17 +11,16 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
+		<h6 class="post-date"><?php the_date( 'l, F j, Y @ h:i a'); ?></h6>
+
 		<?php
 			if ( is_single() ) {
 				the_title( '<h1 class="entry-title">', '</h1>' );
 			} else {
 				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 			}
-
 		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php bripiccari_me_posted_on(); ?>
-		</div><!-- .entry-meta -->
+
 		<?php
 		endif; ?>
 	</header><!-- .entry-header -->
